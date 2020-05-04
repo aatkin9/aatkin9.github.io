2777d421655aaf8ca517a4c1d0dc1bdfac427097
Adam <18adamjames@gmail.com>
2020-05-03 20:39:37 -0400

    build

---
+++ precache-manifest.7b2397fdab1771ceecce08235e6bb90e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ee2ba06fd580b172e6d019ab2b09a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6042705014ec60229b2",
+    "url": "/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "abe2878c374c907821e6",
+    "url": "/static/css/main.db8892fc.chunk.css"
+  },
+  {
+    "revision": "a6042705014ec60229b2",
+    "url": "/static/js/2.48bb1f24.chunk.js"
+  },
+  {
+    "revision": "b5d23eb95f12f07608aa002de8d8de17",
+    "url": "/static/js/2.48bb1f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe2878c374c907821e6",
+    "url": "/static/js/main.ec1a4879.chunk.js"
+  },
+  {
+    "revision": "ef9128d49f1e685f8f27",
+    "url": "/static/js/runtime-main.eefaa529.js"
+  },
+  {
+    "revision": "1e3790ce1bdbde6fafbbc5cf69ad6788",
+    "url": "/static/media/doremi.1e3790ce.webp"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "12f62e741e6b9939dda8754336786572",
+    "url": "/static/media/raindrops.12f62e74.png"
+  },
+  {
+    "revision": "4be7964b9b50dc23fc7207101e5c7322",
+    "url": "/static/media/rowboat.4be7964b.png"
+  }
+]);
